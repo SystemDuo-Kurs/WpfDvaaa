99a513b162cd20f9a23be52115470b954ec60cb9
Dragan Pepic <d.pepic@hotmail.com>
2022-02-17 18:04:59 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDvaaa
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new Radnik();
+        }
+
+        private void MisUpao(object sender, MouseEventArgs e)
+        {
+            dugme.Content = "BUUUUUU";
+        }
+
+        private void MisOtisao(object sender, MouseEventArgs e)
+        {
+            dugme.Content = "Unos";
+        }
+
+        private void Unos(object sender, RoutedEventArgs e)
+        {
+            // Radnik.Ime = ImeTesktboks.Text;
+            //Radnik.Prezime = PrezimeTekstboks.Text;
+        }
+
+        private void Prikaz(object sender, RoutedEventArgs e)
+        {
+            //ImeLabela.Content = Radnik.Ime;
+            //PrezimeLabela.Content = Radnik.Prezime;
+        }
+    }
+}
---
+++ Osoba.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDvaaa
+{
+    internal class Radnik
+    {
+        public string Ime { get; set; } = string.Empty;
+
+        /* Isto kao gore samo duuugacko :/
+         * private string _ime;
+        internal string Ime
+        {
+            get
+            {
+                return _ime;
+            }
+            set
+            {
+                _ime = value;
+            }
+        }*/
+
+        public string Prezime { get; set; } = string.Empty;
+
+        public decimal TrecaPlata { get; set; } = 31;
+
+        private decimal _plata = 30;
+
+        internal decimal GetPlatu()
+        {
+            return _plata;
+        }
+
+        internal void SetPlatu(decimal novaPlata)
+        {
+            if (novaPlata > _plata)
+                _plata = novaPlata;
+        }
+
+        //Isto kao i gore samo cool
+        private decimal _plata2 = 30;
+
+        internal decimal Plata2
+        {
+            get
+            {
+                return _plata2;
+            }
+            set
+            {
+                if (value > _plata2)
+                    _plata2 = value;
+            }
+        }
+    }
+}
